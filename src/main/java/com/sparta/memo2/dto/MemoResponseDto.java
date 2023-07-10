383ac5718df58ecc94ea482bfcc629379830f3d5
yun <dbstjdgur78@naver.com>
2023-07-11 04:30:02 +0900

    JPA Auditing 적용하기

--- src/main/java/com/sparta/memo2/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo2/dto/MemoResponseDto.java
@@ -3,22 +3,24 @@ package com.sparta.memo2.dto;
 import com.sparta.memo2.entity.Memo;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemoResponseDto {
 
     private Long id;
     private String username;
     private String contents;
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
 
     public MemoResponseDto(Memo memo) {
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
+        this.createAt = memo.getCreatedAt();
+        this.modifiedAt = memo.getModifiedAt();
     }
 
-    public MemoResponseDto(Long id, String username, String contents) {
-        this.id = id;
-        this.username = username;
-        this.contents = contents;
-    }
+
 }
