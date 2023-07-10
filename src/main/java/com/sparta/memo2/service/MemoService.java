7fddeb48acc25d2e0dabdb30e59f859f47e44522
yun <dbstjdgur78@naver.com>
2023-07-11 00:13:35 +0900

    강한 결함을 느슨한 결함으로

--- src/main/java/com/sparta/memo2/service/MemoService.java
+++ src/main/java/com/sparta/memo2/service/MemoService.java
@@ -4,7 +4,6 @@ import com.sparta.memo2.dto.MemoRequestDto;
 import com.sparta.memo2.dto.MemoResponseDto;
 import com.sparta.memo2.entity.Memo;
 import com.sparta.memo2.repository.MemoRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
@@ -12,8 +11,8 @@ public class MemoService {
 
     private final MemoRepository memoRepository;
 
-    public MemoService(JdbcTemplate jdbcTemplate) {
-        this.memoRepository = new MemoRepository(jdbcTemplate);
+    public MemoService(MemoRepository memoRepository) {
+        this.memoRepository = new MemoRepository();
     }
 
 
