e8b80fa5b4e9eaa393d66682562751e06500db7c
yun <dbstjdgur78@naver.com>
2023-07-11 04:19:57 +0900

    Spring Data JPA란 무엇일까?

--- src/main/java/com/sparta/memo2/service/MemoService.java
+++ src/main/java/com/sparta/memo2/service/MemoService.java
@@ -5,6 +5,7 @@ import com.sparta.memo2.dto.MemoResponseDto;
 import com.sparta.memo2.entity.Memo;
 import com.sparta.memo2.repository.MemoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -35,34 +36,30 @@ public class MemoService {
     public List<MemoResponseDto> getMemos() {
 
         // DB 조회
-        return memoRepository.findAll();
+        return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
     }
 
-
+    @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
 
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if(memo != null) {
-            // memo 내용 수정
-            memoRepository.update(id, requestDto);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
-
+        Memo memo = findMemo(id);
+        // memo 내용 수정
+        memo.update(requestDto);
+        return id;
     }
 
     public Long deleteMemo(Long id) {
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if(memo != null) {
-            // memo 삭제
-            memoRepository.delete(id);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        Memo memo = findMemo(id);
+        memoRepository.delete(memo);
+        return id;
+
     }
 
+    private Memo findMemo(Long id){
+        return memoRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("선택한 메모는 존재하지 않습니다.")
+        );
+    }
 }
